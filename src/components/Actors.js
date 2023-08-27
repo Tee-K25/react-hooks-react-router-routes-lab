66f06053f73bb1164a66a66b050152e735da9026
Tony <tony.kiplagat@student.moringaschool.com>
2023-08-27 14:08:26 +0300

    Done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  let thyActors = actors.map((actor, index) => {
+    return (
+      <div key={index}>
+        <p>{actor.name}</p>
+        <ul>
+          {actor.movies.map((movie, index) => {
+            return <li key={index}>{movie}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {thyActors}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  let thyMovies = movies.map((movie, index) => {
+    return (
+      <div key={index}>
+        <p>{movie.title}</p>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map((genre, index) => {
+            return <li key={index}>{genre}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {thyMovies}
+    </div>
+  );
 }
 
 export default Movies;
